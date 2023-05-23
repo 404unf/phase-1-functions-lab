5cfb5880587bdacde18ffd258d3645270a9a8341
Anna <34191811+404unf@users.noreply.github.com>
2023-05-22 21:15:47 -0700

    Functions Review

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocale) {
+    const hqLocale = 42
+    let numBlocks = Math.abs(pickupLocale - hqLocale)
+    return numBlocks
+}
+
+
+function distanceFromHqInFeet(locale) {
+    let numBlocks = distanceFromHqInBlocks(locale);
+    let numFeet = numBlocks * 264
+    return numFeet
+}
+
+
+function distanceTravelledInFeet(start,end) {
+    return (Math.abs(end-start))*264
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination)
+    let cost;
+
+    if (distance <= 400)
+        cost = 0
+    else if (distance > 400 && distance <= 2000)
+        cost = (distance-400) * 0.02
+    else if (distance > 2000 && distance <= 2500)
+        cost = 25
+    else if (distance > 2500)
+        return 'cannot travel that far'
+
+    return cost
+}
